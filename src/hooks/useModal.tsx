a1ae2c2c49962aa74a65f4adf36bc341050d7957
duckzuybidan <126694297+duckzuybidan@users.noreply.github.com>
2025-04-30 20:08:05 +0700

    doc for use modal hook

--- src/hooks/useModal.tsx
+++ src/hooks/useModal.tsx
@@ -2,7 +2,36 @@ import { IModalState } from "@/types/modal-type";
 import { create } from "zustand";
 
 
-
+/**
+ * `useModal` is a Zustand store for managing the state of modals in the application.
+ * It supports different modal types (e.g., "DELETE") with associated data and actions.
+ * The store provides functions to open and close modals, with customizable content and submission behavior.
+ * 
+ * The store maintains the following state:
+ * - `isOpen`: A boolean indicating whether the modal is open or not.
+ * - `type`: The type of the modal, which corresponds to a key in the `IModalData` type (e.g., "DELETE").
+ * - `data`: The modal data associated with the current modal type, structured according to the `IModalData` type.
+ * - `onSubmit`: A callback function triggered when the modal's submit action is executed, with optional submit data.
+ * 
+ * The store exposes the following actions:
+ * - `openModal`: Opens the modal, sets its type, data, and submission handler.
+ * - `closeModal`: Closes the modal and resets the state.
+ * 
+ * Example usage:
+ * ```tsx
+ * const { isOpen, openModal, closeModal, type, data, onSubmit } = useModal();
+ * 
+ * // To open the modal
+ * openModal({
+ *   type: 'DELETE',
+ *   data: { title: 'Delete Item', description: 'Are you sure you want to delete this item?' },
+ *   onSubmit: (data) => { console.log('Item deleted:', data); },
+ * });
+ * 
+ * // To close the modal
+ * closeModal();
+ * ```
+ */
 const useModal = create<IModalState>((set) => ({
   isOpen: false,
   type: "",
