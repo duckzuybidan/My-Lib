a00727d95e0d5abfbd497a85a2911617557ff4bc
duckzuybidan <ducduy19012310@gmail.com>
2025-05-02 13:25:57 +0700

    rename

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,22 @@
-
-
 import CustomSelectImage from "./components/custom/custom-select-image";
-import ModalProvider from "./providers/modal-provider";
+import Modals from "./components/modals/modals";
 
 const App = () => {
   return (
     <>
-      <ModalProvider />
+      <Modals />
       <div className="flex items-center justify-center h-screen bg-gray-100">
-        <CustomSelectImage width={100} height={100} onChange={(file) => console.log(file)}>
+        <CustomSelectImage
+          width={100}
+          height={100}
+          onChange={(file) => console.log(file)}
+        >
           <CustomSelectImage.ImageContainer className="shadow-xl inset-shadow-accent-foreground" />
           <CustomSelectImage.EmptyContainer />
         </CustomSelectImage>
       </div>
     </>
-  )
-}
+  );
+};
 
-export default App
+export default App;
